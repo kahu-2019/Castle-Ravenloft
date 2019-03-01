57828cb555fc8d4048d828f37c5be5cfd53200d8
JamesCrimp <james.crimp@gmail.com>
2019-03-01 17:08:46 +1300

    Random encounter function

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,14 @@ const connection = require('./connection')
 function getAllEncounters(db = connection) {
   return db('encounters')
 }
+function getRandomEncounter(db = connection) {
+    
+    // Gets a random number between 1 and 60 (inclusive), as there are 60 encounter cards
+    let num = Math.floor(Math.random()*61)
+    return db('encounters').where('id', num).first()
+}
+
 module.exports = {
-  getAllEncounters
+  getAllEncounters,
+  getRandomEncounter
 }
