fbc36203f44ffde58c7746f5a69fd30a8cde9d54
Karl Brocklehurst <kairakau@gmail.com>
2019-03-03 16:24:40 +1300

    update class to className in SurgeTokens.js

--- client/components/SurgeTokens.jsx
+++ client/components/SurgeTokens.jsx
@@ -1,74 +1,69 @@
-import React, { Component,Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import { getAllCharacters } from '../actions';
 import { connect } from 'react-redux'
 
- class SurgeTokens extends Component {
+class SurgeTokens extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            surgeTokens:2
+        this.state = {
+            surgeTokens: 2
         }
         this.updateHP = this.updateHP.bind(this)
     }
 
-    
 
 
-    updateHP (id) {
+
+    updateHP(id) {
         var character = this.props.characters.find(character => character.id == id)
 
         character.HP = character.SurgeValue
     }
 
-       buttonClicked = () => {
-        this.setState({surgeTokens: this.state.surgeTokens - 1}, () => {
-            
-            
+    buttonClicked = () => {
+        this.setState({ surgeTokens: this.state.surgeTokens - 1 }, () => {
+
+
             this.updateHP(2)
         })
 
 
-       }
-    
-    // useToken = () => {
+    }
 
-    // }
+    render() {
+
+        console.log(this.props.characters)
+        if (this.state.surgeTokens == 2) {
+            return (
+                <div>
+                    <button type="button" className="btn btn-primary" onClick={this.buttonClicked} >Surge Token</button>
+                    <button type="button" className="btn btn-primary" onClick={this.buttonClicked}>Surge Token</button>
+                </div>)
 
 
-  render() {
+        }
+        else if (this.state.surgeTokens == 1) {
+            return (
+                <button type="button" className="btn btn-primary" onClick={this.buttonClicked}>Surge Token</button>
+            )
 
-    console.log(this.props.characters)
-    if(this.state.surgeTokens == 2){
-        return(
-        <div>
-        <button type="button" class="btn btn-primary" onClick={ this.buttonClicked } >Surge Token</button>
-        <button type="button" class="btn btn-primary" onClick={ this.buttonClicked }>Surge Token</button>
-        </div>)
-        
-       
-   }
-   else if (this.state.surgeTokens == 1) {
-       return(
-       <button type="button" class="btn btn-primary" onClick={ this.buttonClicked }>Surge Token</button>
-       )
-       
-   }
+        }
 
-   else {
-       return(
-        ''
-       )
-   }
+        else {
+            return (
+                ''
+            )
+        }
 
-   
 
-}
+
+    }
 
 }
 
 function mapStateToProps(state) {
-    return {characters: state.characters}
+    return { characters: state.characters }
 
 }
 
@@ -76,4 +71,3 @@ function mapStateToProps(state) {
 
 
 export default connect(mapStateToProps)(SurgeTokens);
-  
