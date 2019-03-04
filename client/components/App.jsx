2aeff4fd29254a05730ec333d5c3e9892bc32c35
Karl Brocklehurst <kairakau@gmail.com>
2019-03-05 10:21:18 +1300

    fixing stuff to get Ishan branch up to date with dev

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,28 +6,29 @@ import EncounterTreasure from "./EncounterTreasure";
 import Home from "./Home";
 import Characters from "./Characters";
 import Powers from "./Powers";
+import SurgeTokens from './SurgeTokens';
+import XpCounter from './XpCounter'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Characters />
         <SurgeTokens />
         <XpCounter />
 
-      <div style={{ overflow: 'hidden' }}>
-        <div className="container-fluid">
-          <Router>
-            <Fragment>
-              <Route exact path="/" component={Home} />
-              <Route path="/char-select" component={Characters} />
-              <Route path="/powers/:id" component={Powers} />
-              <Route path="/timetract" component={TimeTract} />
-              <Route path="/encounter-treasure" component={EncounterTreasure} />
-              <Route path="/board" component={Board} />
-            </Fragment>
-          </Router>
-        </div>
+        <div style={{ overflow: 'hidden' }}>
+          <div className="container-fluid">
+            <Router>
+              <Fragment>
+                <Route exact path="/" component={Home} />
+                <Route path="/char-select" component={Characters} />
+                <Route path="/powers/:id" component={Powers} />
+                <Route path="/timetract" component={TimeTract} />
+                <Route path="/encounter-treasure" component={EncounterTreasure} />
+                <Route path="/board" component={Board} />
+              </Fragment>
+            </Router>
+          </div>
         </div>
       </div>
     );
