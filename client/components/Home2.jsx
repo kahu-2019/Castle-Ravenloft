def0e9c3c10e63ead93eaff60960e011de476cac
Sam-Bartos <sambartos@gmail.com>
2019-03-04 21:42:26 +1300

    home page made. too tacky?

--- client/components/Home2.jsx
+++ client/components/Home2.jsx
@@ -8,8 +8,15 @@ class Home2 extends React.Component{
     render(){
         return(
             <div className='home-main'>
-                home2
+                <div className = 'logo-div'>
                 <img className='cr-logo' src='./images/crlogo.png'/>
+                <p className='startbutton'>START</p>
+                </div>
+                <div className='rain'>
+                <img className='dnd-logo' src='./images/dndlogo.png'/>
+                <img className='eda-logo' src='./images/edagames.png'/>
+                </div>   
+                
             </div>
         )
     }
