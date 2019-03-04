e0aec2d85cfaee708b3b7a6a8a799a936987fb13
Karl Brocklehurst <kairakau@gmail.com>
2019-03-05 11:45:18 +1300

    merge into karl and fixed issues

--- migrations/20190301104959_Monsters_Att.js
+++ migrations/20190301104959_Monsters_Att.js
@@ -1,42 +1,16 @@
-<<<<<<< HEAD
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('monsters_att', t => {
-    t.integer('id')
-    t.integer('monster_id')
-    t.string('tactics')
-    t.text('attack_name', 'longtext')
-    t.integer('att')
-    t.integer('dmg')
-    t.integer('miss_dmg')
-    t.text('special', 'longtext')
-  })
-||||||| merged common ancestors
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable('monsters_att', t => {
-    t.integer('id')
-    t.integer('monster_id')
-    t.string('tactics')
-    t.text('attack_name')
-    t.integer('att')
-    t.integer('dmg')
-    t.integer('miss_dmg')
-    t.text('special')
-  })
-=======
-exports.up = function(knex, Promise) {
   return knex.schema.createTable("monsters_att", t => {
     t.integer("id");
     t.integer("monster_id");
     t.string("tactics");
-    t.text("attack_name");
+    t.text("attack_name", 'longtext');
     t.integer("att");
     t.integer("dmg");
     t.integer("miss_dmg");
-    t.text("special");
+    t.text("special", 'longtext');
   });
->>>>>>> dev
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTable("monsters_att");
 };
