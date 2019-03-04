d7f9737c26b595a5903e7e32296d72a8cfa510ec
Karl Brocklehurst <kairakau@gmail.com>
2019-03-05 12:49:02 +1300

    update AC and HP to ac/hp

--- seeds/villian.js
+++ seeds/villian.js
@@ -5,15 +5,15 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('villains').insert([
-        { id: 1, Name: 'Klak, Kobold Sorcerer', Race: 'Reptile', AC: '14', HP: '6' },
-        { id: 2, Name: 'Flesh Goolem', Race: 'Undead ', AC: '14', HP: '10' },
-        { id: 3, Name: 'Howling Hag ', Race: 'Fey', AC: '15', HP: '10' },
-        { id: 4, Name: 'Werewolf', Race: 'Animal', AC: '15', HP: '5' },
-        { id: 5, Name: 'Zombie Dragon', Race: 'Undead', AC: '13', HP: '13' },
-        { id: 6, Name: 'Young Vampire', Race: 'Undead', AC: '16', HP: '8' },
-        { id: 7, Name: 'Zombie Dragon', Race: 'Undead', AC: '13', HP: '13' },
-        { id: 8, Name: 'Gravestorm Dracolich', Race: 'Undead', AC: '14', HP: '20' },
-        { id: 9, Name: 'Count Strahd Von Zaraovich', Race: 'Vampire', AC: '19', HP: '12' }
+        { id: 1, Name: 'Klak, Kobold Sorcerer', Race: 'Reptile', ac: '14', hp: '6' },
+        { id: 2, Name: 'Flesh Goolem', Race: 'Undead ', ac: '14', hp: '10' },
+        { id: 3, Name: 'Howling Hag ', Race: 'Fey', ac: '15', hp: '10' },
+        { id: 4, Name: 'Werewolf', Race: 'Animal', ac: '15', hp: '5' },
+        { id: 5, Name: 'Zombie Dragon', Race: 'Undead', ac: '13', hp: '13' },
+        { id: 6, Name: 'Young Vampire', Race: 'Undead', ac: '16', hp: '8' },
+        { id: 7, Name: 'Zombie Dragon', Race: 'Undead', ac: '13', hp: '13' },
+        { id: 8, Name: 'Gravestorm Dracolich', Race: 'Undead', ac: '14', hp: '20' },
+        { id: 9, Name: 'Count Strahd Von Zaraovich', Race: 'Vampire', ac: '19', hp: '12' }
       ])
     });
 };
