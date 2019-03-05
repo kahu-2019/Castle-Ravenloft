e43c064f4a9ccb94c9a685af03beba583b6e7a75
Katene Jack Cotton <katenejack@gmail.com>
2019-03-06 11:53:56 +1300

    blazing skeleton functionality all working

--- public/game_assets/monster_tactics/blazing_skeleton.js
+++ public/game_assets/monster_tactics/blazing_skeleton.js
@@ -1,8 +1,68 @@
 //Test data
-var heroes = [{id: 1, name:'Karl', AC:20},{id: 2,name:'Sam',AC:15},{id: 3,name:'Miju',AC:10}]
+var playerDetails = {
+    adjacentTile:{
+        adj:false,
+        x:0,
+        y:0
+    },
+    closestPlayer:1,
+    playerTile:[[1,1,1,1],
+                [0,11,0,0],
+                [11,0,0,1],
+                [0,0,0,1]],
+    players:[{id: 1, name:'Karl', AC:20,x:2,y:2},{id: 2,name:'Sam the Gimp',AC:16,x:1,y:3}]
+}
 
-var tileAdjacent = false
+//from pos finder function
 
+var playerPos = {
+    tileX: 1,
+    tileY:1,
+    squareX:3,
+    squareY:3
+}
+
+var monPos = {
+    tileX:2,
+    tileY:2,
+    squareX:3,
+    squareY:3
+}
+
+console.log('first monpos', monPos)
+
+var dataSet = [{x:1,y:1,grid:[
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,2,1,1],
+    [0,0,0,0]
+    ]},
+    {x:1,y:2,grid:[
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,2,0,0],
+            [0,0,0,0]
+        ]},
+        {x:2,y:2,grid:[
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,2,0,0],
+            [0,0,0,0]
+        ]}]
+
+
+//if not adjacent, move monX - playX, monY - playY
+//check if tiles are valid
+
+//values i need
+var closestPlayer = playerDetails.players.find(player => player.id == playerDetails.closestPlayer)
+var heroes = playerDetails.players
+var tileAdjacent = playerDetails.adjacentTile.adj
+
+
+
+console.log(closestPlayer)
+console.log(tileAdjacent)
 // Blazing Skeleton attack stats
 var ballOfFire = {
     att: 7,
@@ -42,11 +102,52 @@ if(tileAdjacent){
     }
 //move closer
 }else{
-    console.log('coming closer')
-    return {
-        movement:{
-            x:0,
-            y:0
+    var monX = monPos.tileX
+    var monY = monPos.tileY
+    var playX = playerPos.tileX
+    var playY = playerPos.tileY
+
+    var newMonPos = {x:monX - playX,y:monY - playY}
+
+    var tileExists = dataSet.find(tile => {
+        tile.x == newMonPos.x && tile.y == newMonPos.y
+        return true
+    })
+    
+    console.log('tile Exists:',tileExists)
+
+    if(tileExists != null || tileExists != undefined){
+        if(tileExists.grid[monPos.squareY - 1][monPos.squareX - 1] == 0 || tileExists.grid[monPos.squareY][monPos.squareX] == 2){
+            monPos.tileX = tileExists.x
+            monPos.tileY = tileExists.y
+        }else{
+            //look for first free space and set monPos to the square as well as the tile
+            let square = {}
+
+            
+            outerLoop: for(let row in tileExists.grid){
+                for(let col in tileExists.grid[row]){
+                    if(tileExists.grid[row][col] == 0 || tileExists.grid[row][col] == 2){
+                        square.x = Number(col)
+                        square.y = Number(row)
+                        break outerLoop
+                    }
+                }
+            }
+
+            monPos = {
+                tileX:tileExists.x,
+                tileY:tileExists.y,
+                squareX:square.x,
+                squareY:square.y
+            }
         }
+
     }
+
+
+    
+    console.log('coming closer', monPos)
+
+    return monPos
 }
