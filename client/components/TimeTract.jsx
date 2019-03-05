30894e4ee51875d16ffffdd39554fdcd5395f992
mijucho <chomiju38@gmail.com>
2019-03-06 11:13:01 +1300

    update time-track css

--- client/components/TimeTract.jsx
+++ client/components/TimeTract.jsx
@@ -12,54 +12,56 @@ class TimeTract extends Component {
   render() {
     return (
       <div className="Time-tract">
-        <div className="time-title">
-          <h5>
-            <strong>TIME TRACT</strong>
-          </h5>
-        </div>
-        <div className="time-containers">
-          <div className="contaniner1 timecontainer1">
-            <div className="contaniner" id="circle">
-              <h5>1</h5>
-              {this.state.sunTrack === 1 && (
-                <img height="40" src="/images/icons/sun.png" />
-              )}
+        <div className="">
+          <div className="time-title text-center">
+            <h5>
+              <strong>TIME TRACT</strong>
+            </h5>
+          </div>
+          <div className="time-containers">
+            <div className="contaniner1 timecontainer1">
+              <div className="contaniner" id="circle">
+                <h5>1</h5>
+                {this.state.sunTrack === 1 && (
+                  <img height="40" src="/images/icons/sun.png" />
+                )}
 
-              <div className="time">
-                <h6 className="morning">
-                  <strong>MORNING</strong>
-                </h6>
+                <div className="time">
+                  <h6 className="morning">
+                    <strong>MORNING</strong>
+                  </h6>
+                </div>
               </div>
-            </div>
 
-            <div className="contaniner" id="circle">
-              <h5>2</h5>
-              {this.state.sunTrack === 2 && (
-                <img height="40" src="/images/icons/sun.png" />
-              )}
-            </div>
-            <div className="contaniner" id="circle">
-              <h5>3</h5>
-              {this.state.sunTrack === 3 && (
-                <img height="40" src="/images/icons/sun.png" />
-              )}
-            </div>
-            <div className="contaniner" id="circle">
-              <h5>4</h5>
-              {this.state.sunTrack === 4 && (
-                <img height="40" src="/images/icons/sun.png" />
-              )}
-            </div>
-            <div className="contaniner" id="circle">
-              <h5>5</h5>
-              {this.state.sunTrack === 5 && (
-                <img height="40" src="/images/icons/sun.png" />
-              )}
-              <span>Strahd wakes up!</span>
-              <div className="time1">
-                <h6>
-                  <strong>SUNSET</strong>
-                </h6>
+              <div className="contaniner" id="circle">
+                <h5>2</h5>
+                {this.state.sunTrack === 2 && (
+                  <img height="40" src="/images/icons/sun.png" />
+                )}
+              </div>
+              <div className="contaniner" id="circle">
+                <h5>3</h5>
+                {this.state.sunTrack === 3 && (
+                  <img height="40" src="/images/icons/sun.png" />
+                )}
+              </div>
+              <div className="contaniner" id="circle">
+                <h5>4</h5>
+                {this.state.sunTrack === 4 && (
+                  <img height="40" src="/images/icons/sun.png" />
+                )}
+              </div>
+              <div className="contaniner" id="circle">
+                <h5>5</h5>
+                {this.state.sunTrack === 5 && (
+                  <img height="40" src="/images/icons/sun.png" />
+                )}
+                <span>Strahd wakes up!</span>
+                <div className="time1">
+                  <h6>
+                    <strong>SUNSET</strong>
+                  </h6>
+                </div>
               </div>
             </div>
           </div>
