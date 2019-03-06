009d3803791ea4fc6e7739d31227987ce170ffb6
mijucho <chomiju38@gmail.com>
2019-03-06 18:58:53 +1300

    update css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import Board from './Board'
+import Board from "./Board";
 import React, { Component, Fragment } from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import TimeTract from "./TimeTract";
@@ -6,17 +6,17 @@ import EncounterTreasure from "./EncounterTreasure";
 import Home2 from "./Home2";
 import Characters from "./Characters";
 import Powers from "./Powers";
-// import Home from "./Home";
+import Home from "./Home";
 
 class App extends Component {
-
   render() {
     return (
-      <div style={{ overflow: 'hidden' }}>
+      <div style={{ overflow: "hidden" }}>
         <div className="container-fluid">
           <Router>
             <Fragment>
               <Route exact path="/" component={Home2} />
+              <Route exact path="/home" component={Home} />
               <Route path="/char-select" component={Characters} />
               <Route path="/powers/:id" component={Powers} />
               <Route path="/timetract" component={TimeTract} />
@@ -30,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default App;
